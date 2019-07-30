f24559bb3a0e874e023f5fa9f2878964e82a7cd7
Roei Bracha <roei635@gmail.com>
2019-07-30 15:13:58 +0300

    Updates

---
+++ precache-manifest.9890b83c3e46a4dcf38baf8fc39dc333.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cbb107c8d8933709d097a23f0c7a089",
+    "url": "/vered-card/index.html"
+  },
+  {
+    "revision": "4b4acbc03d54d2cdb3fc",
+    "url": "/vered-card/static/css/main.a4a67473.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/vered-card/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "4b4acbc03d54d2cdb3fc",
+    "url": "/vered-card/static/js/main.00d835a8.chunk.js"
+  },
+  {
+    "revision": "22431084f57631e3179a",
+    "url": "/vered-card/static/js/runtime~main.63f3e7a0.js"
+  }
+]);
